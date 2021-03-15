bebba8a049b5fe30bc9c0539774340bf0301e29c
Gustavo Mello <gustavo@homedoctor.com.br>
2021-03-15 08:21:49 -0300

    Ajusta configuração do grupo de rotas de 'api' para 'Api'

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['namespace' => 'api'], function () {
+Route::group(['namespace' => 'Api'], function () {
     $exceptMethods = [
         'except' => ['create', 'edit']
     ];
