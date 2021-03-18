ca78cd388aef5b227ef1d79d821d90b998d4aa99
Gustavo Mello <gustavo@homedoctor.com.br>
2021-03-18 16:39:43 -0300

    Finalizando implementação do basic crud controller

--- app/Http/Controllers/Api/BasicCrudController.php
+++ app/Http/Controllers/Api/BasicCrudController.php
@@ -11,6 +11,8 @@ abstract class BasicCrudController extends Controller
 
     protected abstract function rulesStore();
 
+    protected abstract function rulesUpdate();
+
     public function index()
     {
         return $this->model()::all();
@@ -29,5 +31,26 @@ abstract class BasicCrudController extends Controller
         $model = $this->model();
         $keyName = (new $model)->getRouteKeyName();
         return $this->model()::where($keyName, $id)->firstOrFail();
+    }
+
+    public function show($id)
+    {
+        $obj = $this->findOrFail($id);
+        return $obj;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $obj = $this->findOrFail($id);
+        $validatedData = $this->validate($request, $this->rulesUpdate());
+        $obj->update($validatedData);
+        return $obj;
+    }
+
+    public function destroy($id)
+    {
+        $obj = $this->findOrFail($id);
+        $obj->delete();
+        return response()->noContent();
     }
 }
